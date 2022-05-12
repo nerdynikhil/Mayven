6e4287ba8700f4e9c092bdf3c44c28312950eb19
Nikhil Barik <36399086+nerdynikhil@users.noreply.github.com>
2022-05-12 20:53:02 +0530

    initialize application

--- src/main/java/tech/nerdynikhil/Mayven/App.java
+++ src/main/java/tech/nerdynikhil/Mayven/App.java
@@ -1,5 +1,9 @@
 package tech.nerdynikhil.Mayven;
 
+import java.lang.module.Configuration;
+
+import com.mysql.cj.xdevapi.SessionFactory;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Initialized..." );
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        System.out.println(factory);
     }
 }
